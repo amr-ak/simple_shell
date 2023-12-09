386cddc205d8c2aa37b9ebbb1cc57d3344a2d4b0
amr-ak <133274775+amr-ak@users.noreply.github.com>
2023-12-09 23:50:49 +0200

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,75 +1,51 @@
-#include "shell.h"
-
-/**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
- *
- * Return: 1 if interactive mode, 0 otherwise
- */
-int interactive(info_t *info)
-{
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+int is_chain(info_t *info, char *buf, size_t *p) {
+    size_t j = *p;
+    if (buf[j] == '|' && buf[j + 1] == '|') {
+        buf[j] = 0;
+        j++;
+        info->cmd_buf_type = CMD_OR;
+    } else if (buf[j] == '&' && buf[j + 1] == '&') {
+        buf[j] = 0;
+        j++;
+        info->cmd_buf_type = CMD_AND;
+    } else if (buf[j] == ';') {
+        buf[j] = 0;
+        info->cmd_buf_type = CMD_CHAIN;
+    } else {
+        return 0;
+    }
+    *p = j;
+    return 1;
+}
+int replace_vars(info_t *info) {
+    int i;
+    list_t *node;
+    for (i = 1; info->argv[i]; i++) {
+        if (info->argv[i][0] != '$' || !info->argv[i][1]) {
+            continue;
+        }
+        if (!_strcmp(info->argv[i], "$?")) {
+            replace_string(&(info->argv[i]), _strdup(convert_number(info->status, 10, 0)));
+            continue;
+        } else if (!_strcmp(info->argv[i], "$$")) {
+            replace_string(&(info->argv[i]), _strdup(convert_number(getpid(), 10, 0)));
+            continue;
+        }
+        node = node_starts_with(info->env, &(info->argv[i][1]), '=');
+        if (node) {
+            replace_string(&(info->argv[i]), _strdup(_strchr(node->str, '=') + 1));
+        } else {
+            replace_string(&(info->argv[i]), _strdup(""));
+        }
+    }
+    return 0;
 }
 
-/**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
- */
-int is_delim(char c, char *delim)
-{
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
+int replace_string(char **old, char *new) {
+    if (!new) {
+        return 0;
+    }
+    free(*old);
+    *old = new;
+    return 1;
 }
-
-/**
- *_isalpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
- */
-
-int _isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
-
-/**
- *_atoi - converts a string to an integer
- *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
- */
-
-int _atoi(char *s)
-{
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
-
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
-
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
-
-	return (output);
-}
-
